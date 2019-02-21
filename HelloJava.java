57c73f691f2e7ef7ea96c3153104a6dfcd77890c
shaggypande <30983911+shaggypande@users.noreply.github.com>
2019-02-22 00:16:58 +0530

    Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -1,6 +1,6 @@
 class HelloJava {
 	public static void main(String args[]){
-		System.out.Println(" I am your Java Program. Thank you! ");
+		System.out.println(" I am your Java Program. Thank you! ");
 	}
 }
 
