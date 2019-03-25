a1fabf7b2c3d74a56498f7e309678a1876ddf85a
Ubuntu <shaggy.pande@gmail.com>
2019-03-25 04:50:27 +0000

    Devops

---
+++ sample.c
@@ -0,0 +1,6 @@
+#include <stdio.sh>
+int main(){
+printf("Hello! I am C-Program. Thank You!");
+printf ("Branch Push");
+#return 0;
+}
---
+++ sample1.c
@@ -0,0 +1,6 @@
+#include <stdio.sh>
+int main(){
+printf("Hello! I am C-Program. Thank You!");
+#return 0;
+#return 1;
+}
